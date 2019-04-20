e64c2b12ef231f64a9595378a41d0ff5c03065c9
André Sebastian <a.sebastian@posteo.de>
2019-04-20 16:52:25 +0200

    add remove_todo switch case

--- script.js
+++ script.js
@@ -14,13 +14,22 @@ function rootReducer(state=initialState, action) {
       };
       // ADD A TODO
     case 'REMOVE_TODO':
-      // REMOVE A TODO
+      newState.id--;
+      let todos = state.todos.filter(val => val.id !== +action.id)
+      return { ...newState, todos}
     default: return state;
   }
   
 }
 
 const store = Redux.createStore(rootReducer);
+$("ul").on("click", "button", function(e) {
+  store.dispatch({
+    type: "REMOVE_TODO",
+    id: $(event.target).attr("id")
+  });
+  $(event.target).parent().remove();
+})
 
 $(document).ready(function () {
   $("form").on("submit", function(e) {
@@ -30,6 +39,16 @@ $(document).ready(function () {
       type: "ADD_TODO",
       task: newTask
     })
+    let currentState = store.getState();
+    let $newLi = $("<li>", {
+      text: newTask
+    });
+    let $newButton = $("<button>", {
+      text: "X",
+      id: currentState.id
+    });
+    $newLi.append($newButton);
+    $("#todos").append($newLi)
     $("form").trigger("reset");
   })
 });
